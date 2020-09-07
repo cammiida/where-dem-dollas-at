f846774bb915628cce5a36be23a4476c8d2da3a7
Camilla <camillamdalan@gmail.com>
2020-09-07 20:43:05 +0200

    :sparkles::lipstick: Added adobe spectrum

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import "./App.css";
+import { Provider, defaultTheme, Grid, View } from "@adobe/react-spectrum";
+
 import MyComponent from "./components/MyComponent/MyComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MyComponent />
-      </header>
-    </div>
+    <Provider theme={defaultTheme}>
+      <Grid
+        areas={["sidebar content", "sidebar content", "sidebar footer"]}
+        columns={["1fr", "4fr"]}
+        rows={["size-1000", "auto", "size-1000"]}
+        minHeight="100vh"
+        gap="size-100"
+      >
+        <View backgroundColor="blue-600" gridArea="sidebar" />
+        <View backgroundColor="purple-600" gridArea="content">
+          <MyComponent></MyComponent>
+        </View>
+        <View backgroundColor="magenta-600" gridArea="footer" />
+      </Grid>
+    </Provider>
   );
 }
 
--- src/components/MyComponent/MyComponent.tsx
+++ src/components/MyComponent/MyComponent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { View } from "@adobe/react-spectrum";
 
 export interface MyComponentProps {}
 
 const MyComponent: React.FC<MyComponentProps> = (props) => {
-  return <div>My Component</div>;
+  return <View>My Component</View>;
 };
 
 export default MyComponent;
