f7e8e4b0f824b9a8f7c30b4ef563217d40107808
Camilla <camillamdalan@gmail.com>
2020-09-07 20:14:59 +0200

    :tada: First dumb component

---
+++ src/components/MyComponent/MyComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface MyComponentProps {}
+
+const MyComponent: React.FC<MyComponentProps> = (props) => {
+  return <div>My Component</div>;
+};
+
+export default MyComponent;
